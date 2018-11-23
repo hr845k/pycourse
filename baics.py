d36c1d9278b886afa11e6bb1f1027060f90464f2
Hasan Rauf <hr845k@att.com>
2018-11-23 12:16:17 -0500

    pycourse test1

---
+++ baics.py
@@ -0,0 +1,16 @@
+num1 = 1.5
+num2 = 6.3
+
+sum = float (num1)+float(num2)
+
+print ("The sum of two {0} {1} is {2}".format(num1,num2,sum))
+print("hello world")
+
+num = float (input("Enter a number:"))
+
+if num > 0:
+    print("Positive Number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative Number")
